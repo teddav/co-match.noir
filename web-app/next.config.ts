548f42d438ab6a19ca1abf4a8ae3717b24390d38
teddav <teddav@users.noreply.github.com>
2025-05-06 14:42:36 +0200

    lets encrypt cert: sslip.io

--- web-app/next.config.ts
+++ web-app/next.config.ts
@@ -5,7 +5,7 @@ const isProduction = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   env: {
-    NEXT_PUBLIC_API_URL: isProduction ? "https://94.130.176.20:8000" : "http://localhost:8000",
+    NEXT_PUBLIC_API_URL: isProduction ? "https://94.130.176.20.sslip.io:8000" : "http://localhost:8000",
   },
 };
 
